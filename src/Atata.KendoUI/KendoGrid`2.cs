33e1dbb8cb790839ea9bd56b9396b9209e74ebda
YevgeniyShunevych <eshunevich@gmail.com>
2016-11-25 16:32:35 +0200

    Add solution with sources copied from https://github.com/atata-framework/atata/tree/master/src/Atata.KendoUI

---
+++ src/Atata.KendoUI/KendoGrid`2.cs
@@ -0,0 +1,8 @@
+﻿namespace Atata.KendoUI
+{
+    public class KendoGrid<TRow, TOwner> : KendoGrid<TableHeader<TOwner>, TRow, TOwner>
+        where TRow : KendoGridRow<TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+    }
+}
