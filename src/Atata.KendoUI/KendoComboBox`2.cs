e16199aeb0b2e6ef0fbb56f44c7f2978bde77d17
Yevgeniy Shunevych <eshunevich@gmail.com>
2017-01-19 21:28:50 +0200

    Update KendoComboBox`2

--- src/Atata.KendoUI/KendoComboBox`2.cs
+++ src/Atata.KendoUI/KendoComboBox`2.cs
@@ -7,6 +7,9 @@ namespace Atata.KendoUI
     public class KendoComboBox<T, TOwner> : EditableField<T, TOwner>
         where TOwner : PageObject<TOwner>
     {
+        [FindByAttribute("data-role", "combobox", Visibility = Visibility.Hidden)]
+        private TextInput<TOwner> DataInput { get; set; }
+
         protected override T GetValue()
         {
             string valueAsString = Scope.Get(By.TagName("input").Input()).GetValue();
@@ -17,6 +20,16 @@ namespace Atata.KendoUI
         {
             string valueAsString = ConvertValueToString(value);
             Scope.Get(By.TagName("input").Input()).FillInWith(valueAsString);
+        }
+
+        protected override bool GetIsReadOnly()
+        {
+            return DataInput.IsReadOnly;
+        }
+
+        protected override bool GetIsEnabled()
+        {
+            return DataInput.IsEnabled;
         }
     }
 }
