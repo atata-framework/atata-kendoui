40c3236b8b7b35b2441125c8076cb6f1b88c7600
Yevgeniy Shunevych <eshunevich@gmail.com>
2017-01-21 15:18:23 +0200

    Add Atata.KendoUI.TestApp and Atata.KendoUI.Tests projects

--- src/Atata.KendoUI/Properties/AssemblyInfo.cs
+++ src/Atata.KendoUI/Properties/AssemblyInfo.cs
@@ -4,11 +4,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Atata.KendoUI")]
 [assembly: AssemblyDescription("A set of Atata components for Kendo UI HTML Framework.")]
 [assembly: Guid("31fb0589-4ac3-4529-ba13-b353d9a10703")]
-
-[assembly: AssemblyCompany("Yevgeniy Shunevych")]
-[assembly: AssemblyProduct("Atata Framework")]
-[assembly: AssemblyCopyright("Copyright © Yevgeniy Shunevych 2016")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.3.0")]
