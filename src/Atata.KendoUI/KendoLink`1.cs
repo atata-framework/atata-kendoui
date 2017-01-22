64f48c11834b741127d7a6fe6fd5f159d5c96a79
Yevgeniy Shunevych <eshunevich@gmail.com>
2017-01-22 13:59:09 +0200

    Add KendoLink`1 and KendoLink`2

---
+++ src/Atata.KendoUI/KendoLink`1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Atata.KendoUI
+{
+    [ControlDefinition("a", ContainingClass = "k-link", ComponentTypeName = "link", IgnoreNameEndings = "Button,Link")]
+    public class KendoLink<TOwner> : Link<TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+        protected override bool GetIsEnabled()
+        {
+            return Attributes.Class.Value.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries).All(x => x != "k-state-disabled");
+        }
+    }
+}
---
+++ src/Atata.KendoUI/KendoLink`2.cs
@@ -0,0 +1,8 @@
+﻿namespace Atata.KendoUI
+{
+    public class KendoLink<TNavigateTo, TOwner> : KendoLink<TOwner>, INavigable<TNavigateTo, TOwner>
+        where TNavigateTo : PageObject<TNavigateTo>
+        where TOwner : PageObject<TOwner>
+    {
+    }
+}
