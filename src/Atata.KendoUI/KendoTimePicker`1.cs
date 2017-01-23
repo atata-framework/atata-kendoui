50df4e325374bf1143b0f1d7478af5358fef77dc
YevgeniyShunevych <eshunevich@gmail.com>
2017-01-23 13:10:41 +0200

    Add KendoTimePicker`1; Add KendoTimePickerTest

---
+++ src/Atata.KendoUI/KendoTimePicker`1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Atata.KendoUI
+{
+    [ControlDefinition("span", ContainingClass = "k-timepicker", ComponentTypeName = "time picker")]
+    [IdXPathForLabel("[.//input[@id='{0}']]")]
+    [Format("t")]
+    public class KendoTimePicker<TOwner> : EditableField<TimeSpan?, TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+        [FindFirst]
+        private TextInput<TOwner> DataControl { get; set; }
+
+        protected override TimeSpan? GetValue()
+        {
+            string valueAsString = DataControl.Value;
+            return ConvertStringToValue(valueAsString);
+        }
+
+        protected override void SetValue(TimeSpan? value)
+        {
+            string valueAsString = ConvertValueToString(value);
+            Scope.Get(By.TagName("input").Input()).FillInWith(valueAsString);
+        }
+
+        protected override bool GetIsReadOnly()
+        {
+            return DataControl.Attributes.GetValue("readonly") != null;
+        }
+
+        protected override bool GetIsEnabled()
+        {
+            return DataControl.IsEnabled;
+        }
+    }
+}
