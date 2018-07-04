5739be757af057c336158b1550a170e5b1a02311
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-07-04 14:14:07 +0300

    #14 Add KendoCheckBox<TOwner> control

--- src/Atata.KendoUI/KendoCheckBox`1.cs
+++ src/Atata.KendoUI/KendoCheckBox`1.cs
@@ -1,6 +1,6 @@
 ﻿namespace Atata.KendoUI
 {
-    [ControlDefinition("input[@type='checkbox']", ComponentTypeName = "checkbox", Visibility = Visibility.Any, IgnoreNameEndings = "Checkbox,CheckBox,Option")]
+    [ControlDefinition("input[@type='checkbox']", ContainingClass = "k-checkbox", ComponentTypeName = "checkbox", Visibility = Visibility.Any, IgnoreNameEndings = "Checkbox,CheckBox,Option")]
     public class KendoCheckBox<TOwner> : CheckBox<TOwner>
         where TOwner : PageObject<TOwner>
     {
