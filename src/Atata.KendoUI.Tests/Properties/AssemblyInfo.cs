0925628a9730bf88c6fe871adcc1a274a52c02d6
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-09-18 21:58:27 +0300

    Change Atata.KendoUI.Tests project type to new csproj format

--- src/Atata.KendoUI.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Atata.KendoUI.Tests")]
-[assembly: AssemblyDescription("Atata.KendoUI Tests")]
-[assembly: Guid("9bf0be1c-3eb3-451b-9aaf-086d35203ddd")]
