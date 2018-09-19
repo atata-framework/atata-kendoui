9a202bb0b0c499c767ddffa38a19527b403a4fb4
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-09-19 15:45:28 +0300

    #21 Add KendoPopup<TOwner> control

---
+++ src/Atata.KendoUI/KendoPopup`1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Atata.KendoUI
+{
+    [ControlDefinition(ContainingClass = "k-popup", ComponentTypeName = "popup", IgnoreNameEndings = "Popup")]
+    public class KendoPopup<TOwner> : Control<TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+        /// <summary>
+        /// Gets the default animation waiting options.
+        /// Timeout is 5 seconds.
+        /// Interval is 0.1 seconds.
+        /// </summary>
+        public static RetryOptions DefaultAnimationWaitingOptions { get; } = new RetryOptions
+        {
+            Interval = TimeSpan.FromSeconds(.1),
+            Timeout = TimeSpan.FromSeconds(5)
+        };
+
+        public TOwner WaitUntilOpen(RetryOptions waitingOptions = null)
+        {
+            return this.WaitForCssTransitionEnd("open", waitingOptions ?? DefaultAnimationWaitingOptions);
+        }
+
+        public TOwner WaitUntilClosed(RetryOptions waitingOptions = null)
+        {
+            return this.WaitForCssTransitionEnd("close", waitingOptions ?? DefaultAnimationWaitingOptions, SearchOptions.SafelyAtOnce());
+        }
+    }
+}
