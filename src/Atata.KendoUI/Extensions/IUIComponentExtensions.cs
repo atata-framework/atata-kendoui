69fce79682fe6b2174a3ff7f46534ee9339abefb
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-09-19 15:38:21 +0300

    Update IUIComponentExtensions

--- src/Atata.KendoUI/Extensions/IUIComponentExtensions.cs
+++ src/Atata.KendoUI/Extensions/IUIComponentExtensions.cs
@@ -5,20 +5,23 @@ namespace Atata.KendoUI
 {
     internal static class IUIComponentExtensions
     {
-        internal static void WaitForCssTransitionEnd<TOwner>(this IUIComponent<TOwner> component, string transitionName, RetryOptions waitingOptions)
+        internal static TOwner WaitForCssTransitionEnd<TOwner>(this IUIComponent<TOwner> component, string transitionName, RetryOptions waitingOptions, SearchOptions searchOptions = null)
             where TOwner : PageObject<TOwner>
         {
             if (waitingOptions?.Timeout > TimeSpan.Zero)
             {
                 AtataContext.Current.Log.Start($"Wait for {component.ComponentFullName} \"{transitionName}\" CSS transition completion", LogLevel.Trace);
 
-                component.Scope.Try().Until(IsNoCssTransition, waitingOptions);
+                IWebElement element = searchOptions == null ? component.Scope : component.GetScope(searchOptions);
+                element?.Try().Until(HasNoCssTransition, waitingOptions);
 
                 AtataContext.Current.Log.EndSection();
             }
+
+            return component.Owner;
         }
 
-        private static bool IsNoCssTransition(IWebElement element)
+        private static bool HasNoCssTransition(IWebElement element)
         {
             string transitionDuration = element.GetCssValue("transitionDuration");
 
