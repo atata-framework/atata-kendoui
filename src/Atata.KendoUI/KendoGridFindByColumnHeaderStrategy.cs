dee493935406820dba2c95eca700d724a49d4aac
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-10-16 14:56:39 +0300

    #29 KendoGridFindByColumnHeaderStrategy fails for grid inside another grid

--- src/Atata.KendoUI/KendoGridFindByColumnHeaderStrategy.cs
+++ src/Atata.KendoUI/KendoGridFindByColumnHeaderStrategy.cs
@@ -3,7 +3,7 @@
     public class KendoGridFindByColumnHeaderStrategy : FindByColumnHeaderStrategy
     {
         public KendoGridFindByColumnHeaderStrategy()
-            : base("(ancestor::*[contains(concat(' ', normalize-space(@class), ' '), ' k-grid ')])[1]//th")
+            : base("(ancestor::*[contains(concat(' ', normalize-space(@class), ' '), ' k-grid ')])[position() = last()]//th")
         {
         }
     }
