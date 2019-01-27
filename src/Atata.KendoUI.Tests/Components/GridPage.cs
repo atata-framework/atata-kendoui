a06e7e0fdcd688c7e396d180c4445d7a3f91ec2d
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2019-01-27 20:52:05 +0200

    Add KendoGrid_Basic test

---
+++ src/Atata.KendoUI.Tests/Components/GridPage.cs
@@ -0,0 +1,23 @@
+﻿namespace Atata.KendoUI.Tests
+{
+    using _ = GridPage;
+
+    [Url("grid")]
+    public class GridPage : Page<_>
+    {
+        public KendoGrid<CarRow, _> Cars { get; private set; }
+
+        public class CarRow : KendoGridRow<_>
+        {
+            public Text<_> CarMake { get; private set; }
+
+            public Text<_> CarModel { get; private set; }
+
+            public Content<int, _> Year { get; private set; }
+
+            public Text<_> Category { get; private set; }
+
+            public Text<_> AirConditioner { get; private set; }
+        }
+    }
+}
---
+++ src/Atata.KendoUI.Tests/KendoGridTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Atata.KendoUI.Tests
+{
+    public class KendoGridTests : UITestFixture
+    {
+        private GridPage page;
+
+        protected override void OnSetUp()
+        {
+            page = Go.To<GridPage>();
+        }
+
+        [Test]
+        public void KendoGrid_Basic()
+        {
+            var control = page.Cars;
+
+            control.Rows.Count.Should.Equal(21);
+            control.Rows.Should.Contain(x => x.CarMake == "Audi" && x.CarModel == "A4");
+            control.Rows[x => x.CarMake == "Audi" && x.CarModel == "Q7"].Should.Exist();
+        }
+    }
+}
