445a0786098ed97e35353d008df81790094a053a
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-07-20 18:27:32 +0200

    Sync with atata-repository-template and fix code analysis warnings

---
+++ test/Atata.KendoUI.Tests/Extensions/ArrayExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Atata.KendoUI.Tests;
+
+internal static class ArrayExtensions
+{
+    internal static TSource FirstOrDefault<TSource>(this TSource[] source, Predicate<TSource> predicate) =>
+        Array.Find(source, predicate);
+
+    internal static bool Any<TSource>(this TSource[] source, Predicate<TSource> predicate) =>
+        Array.Exists(source, predicate);
+}
